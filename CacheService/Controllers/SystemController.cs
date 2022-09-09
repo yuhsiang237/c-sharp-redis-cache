8d6dfb05f2232e0cb01947915c7569b3087b6f87
yuhsiang237 <umworksite@gmail.com>
2022-09-09 15:34:20 +0800

    Add Redis #2

--- CacheService/Controllers/SystemController.cs
+++ CacheService/Controllers/SystemController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using CacheService.Services.Redis;
+
 namespace CacheService.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class SystemController : ControllerBase
     {
+        private readonly IRedis _redis;
+
+        public SystemController(IRedis redis)
+        {
+            _redis = redis;
+        }
         /// <summary>
         /// Get Status
         /// </summary>
@@ -14,6 +22,22 @@ namespace CacheService.Controllers
         public string GetStatus()
         {
             return "It works.";
+        }
+
+        /// <summary>
+        /// Test Redis
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("TestRedis")]
+        public string TestRedis()
+        {
+            var db = _redis.GetDatabase();
+            var val = "";
+            if (db.StringSet("testKey", "testValue"))
+            {
+                val = db.StringGet("testKey");
+            }
+            return val;
         }
     }
 }
---
+++ CacheService/Services/Redis/_Interfaces/IRedis.cs
@@ -0,0 +1,13 @@
+﻿using StackExchange.Redis;
+
+namespace CacheService.Services.Redis
+{
+    public interface IRedis
+    {
+        /// <summary>
+        /// Get Redis Database
+        /// </summary>
+        /// <returns>IDatabase</returns>
+        public IDatabase GetDatabase();
+    }
+}
